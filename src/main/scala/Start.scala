0cfdb375cc5ee916ab1f7d4c1899ca99d00e0c8a
Udbhav Prasad <32470339+UdbhavPrasad072300@users.noreply.github.com>
2020-10-31 17:10:41 -0400

    clean_up

--- src/main/scala/Start.scala
+++ src/main/scala/Start.scala
@@ -1,9 +1,7 @@
 package main
 
 import org.apache.spark.sql.{SparkSession, DataFrame}
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
-import org.apache.spark.SparkConf
+
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.Column
 import org.apache.spark.ml.feature.Tokenizer
@@ -12,10 +10,12 @@ object Start {
   def main(args: Array[String]): Unit = {
     val spark: SparkSession = SparkSession.builder()
       .master("local[1]")
-      .appName("SparkByExamples.com")
+      .appName("Tokenizing-Text-in-Spark")
       .getOrCreate()
 
     import spark.implicits._
+
+    spark.sparkContext.setLogLevel("ERROR")
 
     val df:DataFrame = spark.read.text("src/main/resources/csv/*.txt")
     df.printSchema()
